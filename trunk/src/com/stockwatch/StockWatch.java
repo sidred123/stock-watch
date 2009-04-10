85c622878c522e8f4445ce9cbdf0d86b63845277
satyajeet.shaligram <satyajeet.shaligram@b375dd2e-22eb-11de-8b5c-13accb87f508>
2009-04-10 00:09:58 +0000

    This contains the link to the second page called details.java. This is a basic version.

--- trunk/src/com/stockwatch/StockWatch.java
+++ trunk/src/com/stockwatch/StockWatch.java
@@ -3,210 +3,332 @@ package com.stockwatch;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gdata.client.finance.FinanceService;
+import com.google.gdata.data.extensions.Money;
+import com.google.gdata.data.finance.DaysGain;
 import com.google.gdata.data.finance.PortfolioEntry;
 import com.google.gdata.data.finance.PortfolioFeed;
+import com.google.gdata.data.finance.PositionData;
+import com.google.gdata.data.finance.PositionEntry;
 import com.google.gdata.util.ServiceException;
-import com.stockwatch.news.NewsItem;
-import com.stockwatch.news.NewsManager;
 
-public class StockWatch extends Activity {
+public class StockWatch extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
-    
+	Hashtable idSymbolHash = new Hashtable();
+	Hashtable SymbolPositionObject = new Hashtable();
 	@Override
     public void onCreate(Bundle savedInstanceState) 
 	{
+		int idcount = 1;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		String $rssUrl = NewsManager.generateURL("NASDAQ", "GOOG");
-		NewsManager newsManager = new NewsManager($rssUrl);
-		try
-		{
-			newsManager.parse();
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		List<NewsItem> newsItems = newsManager.getChannel().getItems();
-		Iterator<NewsItem> newsItemIterator = newsItems.iterator();
-		TableLayout table = (TableLayout)findViewById(R.id.table1);
-		while(newsItemIterator.hasNext())
-		{
-			NewsItem newsItem = newsItemIterator.next();
-			TableRow tableRow1 = new TableRow(this);
-			TextView textView1 = new TextView(this);
-			TableRow tableRow2 = new TableRow(this);
-			TextView textView2 = new TextView(this);
-			TableRow tableRow3 = new TableRow(this);
-			TextView textView3 = new TextView(this);
-			textView1.setText(newsItem.getTitle());
-			textView2.setText(newsItem.getLink());
-			textView3.setText(newsItem.getDescription());
-	  	  	tableRow1.addView(textView1);
-	  	  	tableRow2.addView(textView2);
-	  	  	tableRow3.addView(textView3);
-	  	  	table.addView(tableRow1);
-	  	  	table.addView(tableRow2);
-	  	  	table.addView(tableRow3);
-		}
-		/*      TableLayout t = (TableLayout)findViewById(R.id.table1);
-        
-        PortfolioManager pm = new PortfolioManager("tttemper888@gmail.com", "tempster");
-        List<StockWatchPortfolio> l1 =pm.getPortfolios();
-        if(l1 == null)
+		ScrollView sv = (ScrollView)findViewById(R.id.baseScroll);
+		sv.setBackgroundColor(Color.rgb(0,0, 0));
+		
+		TableLayout t = (TableLayout)findViewById(R.id.table1);
+		t.setColumnStretchable(0, true);
+		t.setColumnStretchable(1, true);
+        t.setColumnStretchable(2, true);
+        t.setColumnStretchable(3, true);
+        t.setBackgroundColor(Color.rgb(0,0, 0));
+        // Let us set the headings.
+        TableRow tr = new TableRow(this);
+        for(int loop=0;loop<5;loop++)
+        {
+	        String name = new String();
+        	switch(loop)
+	        {
+	        case 0: name="STOCK\nname";break;
+	        case 1: name=" ";break;
+	        case 2: name="Shares\nowned";break;
+	        case 3: name="Stock\nPrice";break;
+	        case 4: name="Day's\ngain";break;
+	       
+	        }
+        	
+        	
+		  	TextView tv = new TextView(this);
+		  	tv.setText(name);
+		  	tv.setTextSize(14);
+		  	tr.addView(tv);
+		  	
+        }
+        tr.setBackgroundColor(Color.rgb(102, 0, 0));
+        t.addView(tr);
+	  	
+        PortfolioManager portfolioManager = new PortfolioManager("tttemper888@gmail.com", "tempster");
+        List<StockWatchPortfolio> portfolioList =portfolioManager.getPortfolios();
+        if(portfolioList == null)
         {
         	System.out.print("\n\n\nLIST EMPTY\n\n\n");
         }
-      Iterator<StockWatchPortfolio> ip2 = l1.iterator();
-      ip2.next().getPositions();
-      while(ip2.hasNext())
-      {
-    	  StockWatchPortfolio swpor = ip2.next();
-    	  
-//    	  TableRow tr = new TableRow(this);
-//    	  TextView tv = new TextView(this);
-//    	  tv.setText(swpor.getPortfolioEntry().getTitle().getPlainText());
-//    	  tr.addView(tv);
-//    	  t.addView(tr);
-    	  System.out.println(swpor.getPortfolioEntry().getTitle().getPlainText());
-    	  Set<StockWatchPosition> s1 = swpor.getPositions();
-    	  System.out.println("Size: "+s1.size());
-    	  
-    	  Iterator<StockWatchPosition> i1 = s1.iterator();
-    	  while(i1.hasNext())
-    	  {
-    		 StockWatchPosition swpos = i1.next();
-    		 TableRow tr1 = new TableRow(this);
-       	  	TextView tv1 = new TextView(this);
-       	  	tv1.setText(swpos.getPositionEntry().getSymbol().getFullName());
-       	  	TextView tv2 = new TextView(this);
-       	  	tv2.setText(Double.toString(swpos.getStockCount()));
-       	  	PositionEntry temp_pe = swpos.getPositionEntry(); 
-       	  	PositionData pdata = temp_pe.getPositionData();		 
-       	  	TextView tv3 = new TextView(this);	
-       //	  	DaysGain m = pdata.getDaysGain();
-//       	  	
-       //	  	tv3.setText(m.toString());
-       	  	tr1.addView(tv1);
-       	  	tr1.addView(tv2);
-    	  	tr1.addView(tv3);
-       	  	t.addView(tr1);
-       	  	
-       	  	if (pdata.getDaysGain() == null) 
-       	  	{
-       	  		System.out.println("\t\tDay's Gain not specified");
-       	  	} 
-       	  	else 
-       	  	{
-       	  		System.out.println(pdata.getDaysGain().getMoney().size());
-       	  		for (int i = 0; i < pdata.getDaysGain().getMoney().size(); i++) 
+        Iterator<StockWatchPortfolio> portfolioListIterator = portfolioList.iterator();
+        while(portfolioListIterator.hasNext())
+        {
+        	StockWatchPortfolio portfolio = portfolioListIterator.next();
+//        	TableRow tableRow = new TableRow(this);
+//        	TextView textView = new TextView(this);
+//        	textView.setText(portfolio.getPortfolioEntry().getTitle().getPlainText());
+        	Set<StockWatchPosition> positionSet = portfolio.getPositions();
+//        	tableRow.addView(textView);
+//        	t.addView(tableRow);
+        	
+        	Iterator<StockWatchPosition> positionSetIterator = positionSet.iterator();
+        	while(positionSetIterator.hasNext())
+        	{
+        		StockWatchPosition position = positionSetIterator.next();
+        		
+        		TableRow tr1 = new TableRow(this);
+       	  		TextView tv1 = new TextView(this);
+       	  		String name = new String(position.getPositionEntry().getSymbol().getFullName());
+       	  		String sname = new String(position.getPositionEntry().getSymbol().getSymbol());
+       	  		if(name.length()>15)
        	  		{
-       	  			Money money = pdata.getDaysGain().getMoney().get(i);
-       	  			System.out.printf("\t\tThis position made %.2f %s today.\n", money.getAmount(), money.getCurrencyCode());
+       	  			name = name.substring(0,15) + "..";
        	  		}
-           }
-           if (pdata.getGain() == null) 
-           {
-             System.out.println("\t\tTotal Gain not specified");
-           } 
-           else 
-           {
-             for (int i = 0; i < pdata.getGain().getMoney().size(); i++) 
-             {
-            	 Money money = pdata.getGain().getMoney().get(i);
-            	 System.out.printf("\t\tThis position has a total gain of %.2f %s.\n", money.getAmount(), money.getCurrencyCode());
-             }
-           }
-           if (pdata.getMarketValue() == null) 
-           {
-        	   System.out.println("\t\tMarket Value not specified");
-           } 
-           else 
-           {
-        	   for (int i = 0; i < pdata.getMarketValue().getMoney().size(); i++) 
-        	   {
-        		   Money money = pdata.getMarketValue().getMoney().get(i);
-        		   System.out.printf("\t\tThis position is worth %.2f %s.\n", money.getAmount(), money.getCurrencyCode());
-        	   }
-           	}
-         }
-        
-        
-       
-	      
-	      
-	      
-    
-        
+       	  		tv1.setText(sname);
+       	  		// Let us assign an ID to this text box and store relation in the hash table
+       	  		tv1.setId(idcount);
+       	  		idSymbolHash.put(new Integer(idcount), sname);
+       	  		idcount++;
+       	  		tv1.setOnClickListener(this);
+       	  		
+       	  		
+       	  		SymbolPositionObject.put(sname, position);
+       	  		
+       	  		TextView tv2 = new TextView(this);
+       	  		TextView tv3 = new TextView(this);
+       	  		TextView tv4 = new TextView(this);
+       	  		TextView tv5 = new TextView(this);
+       	  		tr1.addView(tv1);
+       	  		tr1.addView(tv2);
+       	  		tr1.addView(tv3);
+       	  		tr1.addView(tv4);
+       	  		tr1.addView(tv5);
+       	  		t.addView(tr1);
+       	  		tv1.setTextSize(14);
+       	  		tv2.setTextSize(11);
+       	  		tv3.setTextSize(14);
+       	  		tv4.setTextSize(14);
+       	  		tv5.setTextSize(14);
+       	  		
+       	  		// Set other IDs as well
+       	  	tv2.setId(idcount);
+   	  		idSymbolHash.put(new Integer(idcount), sname);
+   	  		idcount++;
+   	  		tv3.setId(idcount);
+	  		idSymbolHash.put(new Integer(idcount), sname);
+	  		idcount++;
+	  		tv4.setId(idcount);
+   	  		idSymbolHash.put(new Integer(idcount), sname);
+   	  		idcount++;
+   	  		tv5.setId(idcount);
+	  		idSymbolHash.put(new Integer(idcount), sname);
+	  		idcount++;
+	  		
+	  		tv2.setOnClickListener(this);
+	  		tv3.setOnClickListener(this);
+	  		tv4.setOnClickListener(this);
+	  		tv5.setOnClickListener(this);
+       	  		
+       	  		
+       	  		tv2.setText(name);
+       	  		
+       	  	int count = (int)position.getStockCount();
+   	  		tv3.setText(Integer.toString(count));
+   	  		PositionEntry positionEntry = position.getPositionEntry(); 
+   	  		PositionData pdata = positionEntry.getPositionData();		 
+   	  		
+       	  	
+       	  		
+       	  	if (pdata.getMarketValue() == null)
+	       	 {
+	       	       System.out.println("\t\tMarket Value not specified");
+	       	 }
+	       	 else
+	       	 {
+	       	 		for (int i = 0; i < pdata.getMarketValue().getMoney().size(); i++) {
+	       	 		Money m = pdata.getMarketValue().getMoney().get(i);
+	       	         System.out.printf("\t\tThis position is worth %.2f %s.\n", m.getAmount(), m.getCurrencyCode());
+	       	         String lastprice = Double.toString(m.getAmount()/position.getStockCount());
+	       	         String value = lastprice; // + " " + m.getCurrencyCode();
+	       	         tv4.setText(value);
+	       	 		}
+	       	 }
+       	  		
+       	  		
+       	  	
+       	  		if (pdata.getDaysGain() == null) 
+       	  		{
+       	  			System.out.println("\t\tDay's Gain not specified");
+       	  		} 
+       	  		else 
+       	  		{	
+       	  			int limit=5;
+       	  			System.out.println(pdata.getDaysGain().getMoney().size());
+       	  			for (int i = 0; i < pdata.getDaysGain().getMoney().size(); i++) 
+       	  			{
+       	  				Money money = pdata.getDaysGain().getMoney().get(i);
+       	  				
+       	  				
+       	  				String gain = Double.toString(money.getAmount()/position.getStockCount());
+	       	  			if(gain.length()<5)
+	   	  				{
+	   	  					limit = gain.length();
+	   	  				}
+       	  				
+       	  				String value = gain.substring(0, limit);// + " " + money.getCurrencyCode();
+       	  				
+       	  				tv5.setText(value);
+       	  				System.out.printf("\t\tThis position made %.2f %s today.\n", money.getAmount(), money.getCurrencyCode());
+       	  			}
+       	  		}
+       	  		if (pdata.getGain() == null) 
+       	  		{
+       	  			System.out.println("\t\tTotal Gain not specified");
+       	  		} 
+       	  		else 
+       	  		{
+       	  			for (int i = 0; i < pdata.getGain().getMoney().size(); i++) 
+       	  			{
+       	  				Money money = pdata.getGain().getMoney().get(i);
+       	  				System.out.printf("\t\tThis position has a total gain of %.2f %s.\n", money.getAmount(), money.getCurrencyCode());
+       	  			}
+       	  		}
+       	  		if (pdata.getMarketValue() == null) 
+       	  		{
+       	  			System.out.println("\t\tMarket Value not specified");
+       	  		} 
+       	  		else 
+       	  		{
+       	  			for (int i = 0; i < pdata.getMarketValue().getMoney().size(); i++) 
+       	  			{
+       	  				Money money = pdata.getMarketValue().getMoney().get(i);
+       	  				System.out.printf("\t\tThis position is worth %.2f %s.\n", money.getAmount(), money.getCurrencyCode());
+       	  			}
+       	  		}
+        	}
         }
+        String string = new String("Please Click on stocks to get \nadditional Information...");
+        Context context = getApplicationContext();
+		int duration = Toast.LENGTH_LONG;
+		Toast toast = Toast.makeText(context, string, duration);
+		toast.show();
         
-        
-        
-        
-//		setListAdapter(new ArrayAdapter<String>(this, R.layout.notes_row, message));
-//		getListView().setTextFilterEnabled(true);*/
-    }
+    }	
 	
-	public String[] getPortfolios()
+	public void onClick(View v)
 	{
-		System.out.println("Authenticating...");
-        FinanceService myService = new FinanceService("MyService");
-        GoogleAccountConnector gac = new GoogleAccountConnector();
-		try
-		{
-			String authToken = gac.getAuthToken("tttemper888@gmail.com", "tempster");
-			//myService.getAuthToken("tttemper888@gmail.com", "tempster", "", "", "MyService", "SubObjective2");
-			//myService.setUserCredentials("tttemper888@gmail.com", "tempster");
-			System.out.println(authToken);
-			myService.setUserToken(authToken);
-			System.out.println("Successfully authenticated");
-		}
-		catch(Exception e)
-		{
-			System.out.println("Authentication Failure");
-			e.printStackTrace();
-			String[] str = new String[1];
-			str[1] = e.getMessage();
-			return str;
-		}
+		Integer id = new Integer(v.getId());
+		// Let us recover the symbol from this
+		String symbol = (String)idSymbolHash.get(id);
+		Context context = getApplicationContext();
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText(context, symbol, duration);
+		//toast.show();
 		
-		System.out.println("Getting Portfolios.");
-		String[] portfolios = new String[2];
-		try 
-		{
-			PortfolioFeed portfolioFeed = myService.getFeed(new URL("http://finance.google.com/finance/feeds/default/portfolios"), PortfolioFeed.class);
-			System.out.println("I am here.");
-			for(int i = 0; i < portfolioFeed.getEntries().size(); i++)
-			{
-				PortfolioEntry portfolioEntry = portfolioFeed.getEntries().get(i);
-				portfolios[i] = portfolioEntry.getTitle().getPlainText();
-			}
-		} 
-		catch (MalformedURLException e) 
-		{
-			e.printStackTrace();
-		} 
-		catch (IOException e) 
-		{
-			e.printStackTrace();
-		} 
-		catch (ServiceException e) 
-		{
-			e.printStackTrace();
-		}
-		return portfolios;
+		// Obtaining the position object first
+		StockWatchPosition position = (StockWatchPosition)SymbolPositionObject.get(symbol);
+	
+		
+		// Getting the vars
+		String fname = new String(position.getPositionEntry().getSymbol().getFullName());
+		int count = (int)position.getStockCount();	
+		PositionEntry positionEntry = position.getPositionEntry(); 
+  		PositionData pdata = positionEntry.getPositionData();
+  		String exchange = positionEntry.getSymbol().getExchange();
+  		String percentgain = Double.toString(pdata.getGainPercentage() * 100.0);
+  		String originalprice="",lastprice="",daysgain="",totalgain="";
+  		if(!(pdata.getCostBasis() == null))
+	  		{
+   	  		for (int i = 0; i < pdata.getCostBasis().getMoney().size(); i++)
+   	  		{
+   	         Money m = pdata.getCostBasis().getMoney().get(i);
+   	         System.out.printf("\t\tThis position cost %.2f %s.\n", m.getAmount(), m.getCurrencyCode());
+   	         originalprice = Double.toString(m.getAmount()/position.getStockCount());
+   	  		}
+	  			
+	  		}
+	  		
+	  		if(!(pdata.getMarketValue() == null))
+	  		{
+	  			for (int i = 0; i < pdata.getMarketValue().getMoney().size(); i++) 
+	  			{
+   	  			Money m = pdata.getMarketValue().getMoney().get(i);
+       	        System.out.printf("\t\tThis position is worth %.2f %s.\n", m.getAmount(), m.getCurrencyCode());
+       	        lastprice = Double.toString(m.getAmount()/position.getStockCount());
+       	    }
+	  		}// marketvalue ends
+	  	
+   	  	if(!(pdata.getDaysGain() == null))
+   	  	{
+   	  		for (int i = 0; i < pdata.getDaysGain().getMoney().size(); i++) 
+   	  		{
+	  				Money money = pdata.getDaysGain().getMoney().get(i);
+	  				daysgain = Double.toString(money.getAmount()/position.getStockCount());
+   	  		}
+   	  	}// days gain ends
+   	  	
+   	  	if(!(pdata.getGain() == null))
+   	  	{
+       	  	for (int i = 0; i < pdata.getGain().getMoney().size(); i++) 
+	  			{
+	  				Money money = pdata.getGain().getMoney().get(i);
+	  				System.out.printf("\t\tThis position has a total gain of %.2f %s.\n", money.getAmount(), money.getCurrencyCode());
+	  				totalgain = Double.toString(money.getAmount()/position.getStockCount());
+	  			}
+   	  		
+   	  	}
+	
+		
+		
+		// Let us go to the details page
+		Intent intent = new Intent();
+		intent.setClassName("com.stockwatch","com.stockwatch.details");
+		intent.putExtra("stockcode", symbol);
+		intent.putExtra("fullname", fname);
+		intent.putExtra("stockcount", Integer.toString(count));
+		intent.putExtra("exchange",exchange );
+		intent.putExtra("percentgain",percentgain );
+		intent.putExtra("lastprice",lastprice );
+		intent.putExtra("originalprice",originalprice );
+		intent.putExtra("daysgain",daysgain );
+		intent.putExtra("totalgain",totalgain);
+		startActivity(intent);
+		
+		
 	}
 
-
+	public void onResume()
+	{
+		super.onResume();
+		String string = new String("Please Click on stocks to get \nadditional Information...");
+        Context context = getApplicationContext();
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText(context, string, duration);
+		toast.show();
+		
+	}
 }
